73570706bdb5fdff19c7b4eb15a1b4ea0e05956b
Senbiki-Akifumi <1400744+wiredrawing@users.noreply.github.com>
2024-04-06 22:35:58 +0900

    サーバーアプリとクライアントアプリを同一の実行ファイルで可能になるように修正

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"errors"
+	"flag"
 	"fmt"
 	"log"
 	"net"
-	"reflect"
+	"os"
+	"strings"
 	"time"
 )
 
@@ -18,18 +21,21 @@ type ClientUnit struct {
 }
 
 type ClientManager struct {
-	clientList map[net.Addr]ClientUnit
+	clientList map[net.Addr]*ClientUnit
 }
 
-func (cm *ClientManager) addClient(client ClientUnit) {
-	fmt.Printf("---------%v---------------", reflect.TypeOf(client.connection.RemoteAddr()))
-	fmt.Printf("---------%v---------------", client.connection.RemoteAddr())
-	cm.clientList[client.connection.RemoteAddr()] = client
+func (cm *ClientManager) addClient(refClient *ClientUnit) (bool, error) {
+	var address net.Addr = refClient.connection.RemoteAddr()
+	if _, ok := cm.clientList[address]; ok != true {
+		cm.clientList[address] = refClient
+		return true, nil
+	}
+	return false, errors.New("クライアントは既に存在しています")
 }
-func (cm *ClientManager) removeClient(client ClientUnit) {
+func (cm *ClientManager) removeClient(client *ClientUnit) {
 	delete(cm.clientList, client.connection.RemoteAddr())
 }
-func (cm *ClientManager) exists(client ClientUnit) bool {
+func (cm *ClientManager) exists(client *ClientUnit) bool {
 	if _, ok := cm.clientList[client.connection.RemoteAddr()]; ok {
 		return ok
 	} else {
@@ -39,111 +45,154 @@ func (cm *ClientManager) exists(client ClientUnit) bool {
 
 // TCPクライアントを管理する構造体を作成
 var clientManager = ClientManager{
-	clientList: make(map[net.Addr]ClientUnit),
+	clientList: make(map[net.Addr]*ClientUnit),
 }
 
 func main() {
-	_, err := net.ResolveTCPAddr("tcp", ":10080")
-	if err != nil {
-		log.Fatal(err)
-	}
-	// Create a listener on TCP port 10080
-	listener, err := net.Listen("tcp", ":10080")
-	if err != nil {
-		panic(err)
-	}
+	// コマンドライン引数を取得
+	asClient := flag.Int("client", 0, "TCPクライアントとして実行する場合")
+	flag.Parse()
 
-	defer func() {
-		err := listener.Close()
+	if (*asClient) == 1 {
+		// 読み取らせるマックスbyte数
+		const BYTE_SIZE = 1024
+		fmt.Println("TCPクライアントの起動---")
+		// TCPクライアントとして起動させた場合
+		addr := &net.TCPAddr{
+			IP:   net.ParseIP("127.0.0.1"),
+			Port: 10080,
+		}
+		connection, err := net.DialTCP("tcp", nil, addr)
 		if err != nil {
 			panic(err)
 		}
-	}()
 
-	for {
-		connection, err := listener.Accept()
+		fetchReceiveBufferFromServer := func() {
+			for {
+				// 読み取り開始
+				//fmt.Println("TCPサーバーからの読み取り開始---")
+				readBytes := make([]byte, BYTE_SIZE)
+				_ = connection.SetReadDeadline(time.Now().Add(5 * time.Second))
+				for {
+					buffer := make([]byte, BYTE_SIZE)
+					size, err := connection.Read(buffer)
+					if err != nil {
+						// timeoutエラーを検出
+						if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
+							//fmt.Printf("Timeout時間を延慶")
+							_ = connection.SetReadDeadline(time.Now().Add(5 * time.Second))
+						} else {
+							panic(err)
+						}
+					}
+					// 読み取ったバイト数のみスライスする
+					buffer = buffer[:size]
+					readBytes = append(readBytes, buffer...)
+					if (size < BYTE_SIZE) && (size > 0) {
+						break
+					}
+				}
+				fmt.Printf("サーバーからのレスポンス-- %v", string(readBytes))
+			}
+			connection.Write([]byte("TCPクライアントから接続 -------------------------->"))
+		}
+		go fetchReceiveBufferFromServer()
+
+		for {
+			var prompt = " >>> "
+			fmt.Printf(prompt)
+			scanner := bufio.NewScanner(os.Stdin)
+			result := scanner.Scan()
+			if result == true {
+				var input string = scanner.Text()
+				// 入力したテキストをサーバーへ送信
+				if _, err := connection.Write([]byte(input)); err != nil {
+					panic(err)
+				}
+			}
+		}
+
+	} else {
+		tcp, err := net.ResolveTCPAddr("tcp", ":10080")
+		if err != nil {
+			log.Fatal(err)
+		}
+		// Create a listener on TCP port 10080
+		listener, err := net.ListenTCP("tcp", tcp)
 		if err != nil {
 			panic(err)
 		}
-		// 新規接続クライアントオブジェクトを作成
-		var client = ClientUnit{
-			clientName: "",
-			connection: connection,
-		}
 
-		isExists := clientManager.exists(client)
-		if isExists == false {
-			clientManager.addClient(client)
-		}
+		defer func() {
+			err := listener.Close()
+			if err != nil {
+				panic(err)
+			}
+		}()
 
-		go handlingConnection(client)
+		for {
+			connection, err := listener.Accept()
+			if err != nil {
+				panic(err)
+			}
+
+			// 新規接続クライアントオブジェクトを作成
+			var client *ClientUnit = new(ClientUnit)
+			client.clientName = ""
+			client.connection = connection
+
+			isExists := clientManager.exists(client)
+			fmt.Printf("isExists => %v\n", isExists)
+			if isExists == false {
+				clientManager.addClient(client)
+			}
+			fmt.Printf("クライアントリストclientManager: %v\n", clientManager)
+			fmt.Printf("クライアントリストclientManager.clientList: %v\n", clientManager.clientList)
+			go handlingConnection(client)
+		}
 	}
 }
 
-func handlingConnection(clientUnit ClientUnit) error {
+func handlingConnection(clientUnit *ClientUnit) error {
 	var c net.Conn = clientUnit.connection
 	printf("%v", c.RemoteAddr())
-	var fromAddress string
-	c.Write([]byte("あなたのお名前を最初に入力してください\n"))
+	c.Write([]byte("<< あなたのお名前を最初に入力してください >>\n"))
+	c.Write([]byte("<< ユーザー一覧を表示する場合は、usersと入力してください >>\n"))
 	printf("読み取り開始\n")
-	c.SetReadDeadline(time.Now().Add(1 * time.Second))
+	if err := c.SetReadDeadline(time.Now().Add(1 * time.Second)); err != nil {
+		panic(err)
+	}
 	for {
 		stackBuffer := make([]byte, 0, 1024)
 		buffer := make([]byte, 32)
 		//printf("待ち受けかいし ")
 		for {
-			// Read the incoming connection into the buffer
 
 			size, err := c.Read(buffer)
-			var opError *net.OpError
+			//var opError *net.OpError
 			if err != nil {
-				if errors.As(err, &opError) == true {
-					c.SetReadDeadline(time.Now().Add(1 * time.Second))
-					//printf("これはタイムアウトのエラーです")
-				} else {
-					panic(err)
+				//if errors.Is(err, &net.OpError{}) {
+				//	printf("IsメソッドがTrueを返す場合")
+				//}
+				//e := errors.Unwrap(err)
+				//printf("e ===> %v ]]]]\n", e)
+				//printf("エラー発生 %v ===================== \n", reflect.TypeOf(e))
+				//fmt.Printf("errors.Unwrap(err) => %v >>> \n", errors.Unwrap(err))
+				//fmt.Printf("reflect.TypeOf(err) => %v >>> \n", reflect.TypeOf(err))
+				//fmt.Printf("reflect.TypeOf(err).Comparable() ==> %v >>> \n", reflect.TypeOf(err).Comparable())
+				var ope = new(net.OpError)
+				if ok := errors.As(err, &(ope)); ok {
+				}
+				if _, ok := err.(*net.OpError); ok {
+					//fmt.Printf(" エラー時 %v\n", reflect.TypeOf(err))
+					// 型アサーション成功時<エラー型>が指定したエラーの場合
+					//fmt.Printf(" タイムアウトエラー時 %v\n", err)
+					if err := c.SetReadDeadline(time.Now().Add(1 * time.Second)); err != nil {
+						panic(err)
+					}
 				}
 			}
 
-			//} else {
-			//	printf("これはタイムアウトのエラーではありません")
-			//	panic(err)
-			//	break
-			//}
-			//if err == io.EOF {
-			//	printf("EOF")
-			//	break
-			//}
-			//if err != nil {
-			//	printf("%v", reflect.TypeOf(err))
-			//	printf("%v", err.Error())
-			//
-			//}
-			//if err != nil {
-			//	if err == io.EOF {
-			//		break
-			//		// io.EOF, etc
-			//	} else if err.(*net.OpError).Timeout() {
-			//		// no status msgs
-			//		// note: TCP keepalive failures also cause this; keepalive is on by default
-			//		continue
-			//	} else {
-			//		panic(err)
-			//	}
-			//}
-			//if err != nil {
-			//	//printf("err => %v\n", err)
-			//	break
-			//}
-
-			//if size == 0 {
-			//	printf(string(buffer))
-			//	printf("size => %v\n", size)
-			//	printf("読み取り完了")
-			//	break
-			//printf("読み込んだbyteサイズ ->"+
-			//	" %v", size)
-			//printf("スライシング前%v", buffer)
 			buffer = buffer[:size]
 			//printf("スライシング後%v", buffer)
 			stackBuffer = append(stackBuffer, buffer...)
@@ -151,34 +200,28 @@ func handlingConnection(clientUnit ClientUnit) error {
 				break
 			}
 		}
-		//panic(",あああああああああああ")
-		//buffer := make([]byte, 512)
-		////for {
-		//if size, err := c.Read(buffer); err != nil {
-		//	printf("size => %v\n", size)
-		//	printf("err => %v\n", err)
-		//	printf("buffer => %v\n", buffer)
-		//	printf("%v", err)
-		//	break
-		//} else {
-		//	buffer = buffer[:size]
-		//	printf("buffer => %v\n", buffer)
-		//	stackBuffer = append(stackBuffer, buffer...)
-		//	if size < 32 {
-		//		//break
-		//	}
-		//}
-		////}
-		//panic("stop")
+
 		if len(stackBuffer) == 0 {
-			//printf("stackBuffer => %v\n", stackBuffer)
 			continue
 		}
 		recievedMessage := string(stackBuffer)
+
+		if recievedMessage == "users" {
+			users := make([]string, len(clientManager.clientList))
+			for address, value := range clientManager.clientList {
+				s := fmt.Sprintf("[%s]:%s", address, value.clientName)
+				users = append(users, s)
+			}
+			fmt.Printf("接続中のユーザー: %v\n", users)
+			connectedUsers := strings.Join(users, "\n")
+			fmt.Printf("接続中のユーザー: %v\n", connectedUsers)
+			c.Write([]byte("接続中のユーザー: " + connectedUsers + "\n"))
+			continue
+		}
 		// ClientUnit構造体のclientNameが空の場合は、クライアント名を登録
-		if len(clientUnit.clientName) == 0 {
+		if len((*clientUnit).clientName) == 0 {
 			// 名前を設定
-			clientUnit.clientName = recievedMessage
+			(*clientUnit).clientName = recievedMessage
 			c.Write([]byte("ようこそ" + clientUnit.clientName + "さん\n"))
 
 			// 発信者以外のユーザーに接続開始した旨を通知
@@ -195,11 +238,6 @@ func handlingConnection(clientUnit ClientUnit) error {
 				}
 			}
 		}
-		//printf("%v\n", stackBuffer)
-		//printf("recievedMessage => <%s>\n", recievedMessage)
-		//printf("recievedMessage => <%v>\n", recievedMessage)
-		//panic("!!!!!!!!!!!!!!")
-
 		// exit or end を入力されたときはクライアントの接続を切断
 		if recievedMessage == "exit" || recievedMessage == "end" {
 			printf("クライアントリストから削除")
@@ -212,8 +250,9 @@ func handlingConnection(clientUnit ClientUnit) error {
 			return nil
 		} else {
 			// Print the request to the console
-			printf("接続元アドレス: %s\n", fromAddress)
-			printf("受信したデータ: %s\n", string(stackBuffer))
+			printf("%s: %s\n", clientUnit.clientName, string(stackBuffer))
+			//printf("接続元アドレス: %s\n", fromAddress)
+			//printf("受信したデータ: %s\n", string(stackBuffer))
 			//var reply string = fmt.Sprintf("%sさんが発言しました: %s\n", clientUnit.clientName, string(stackBuffer))
 			//c.Write([]byte(reply))
 		}
